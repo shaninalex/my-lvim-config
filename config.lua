74ad6dd4ce119dddde656903971bad7d65425ffa
shaninalex <shanin.x@gmail.com>
2023-04-25 21:54:58 +0300

    update config

--- config.lua
+++ config.lua
@@ -2,24 +2,28 @@
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.relativenumber = true
+vim.opt.swapfile = false
+
 
 -- general
 lvim.log.level = "info"
 lvim.format_on_save = {
-  enabled = true,
-  pattern = "*.lua",
-  timeout = 1000,
+    enabled = true,
+    pattern = "*.lua, *.go, *.c, *.cpp, *.js, *.ts",
+    timeout = 1000,
 }
 -- to disable icons and use a minimalist setup, uncomment the following
--- lvim.use_icons = false
+lvim.use_icons = false
 
 -- keymappings <https://www.lunarvim.org/docs/configuration/keybindings>
 lvim.leader = "space"
 -- add your own keymapping
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 
--- -- Change theme settings
-lvim.colorscheme = "gruvebox"
+lvim.keys.normal_mode["<leader><tab>"] = "<cmd>DockerToolsToggle<cr>"
+
+-- Change theme settings
+lvim.colorscheme = "tokyonight-night"
 
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
@@ -30,39 +34,11 @@ lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 -- Automatically install missing parsers when entering buffer
 lvim.builtin.treesitter.auto_install = true
 
--- -- Additional Plugins <https://www.lunarvim.org/docs/plugins#user-plugins>
+
+-- Additional Plugins <https://www.lunarvim.org/docs/plugins#user-plugins>
 lvim.plugins = {
-    { "ellisonleao/gruvbox.nvim" }
+    { 'akinsho/toggleterm.nvim' },
+    { 'folke/tokyonight.nvim' },
+    { 'kkvh/vim-docker-tools' }
 }
-
-require("gruvbox").setup({
-    undercurl = true,
-    underline = true,
-    bold = true,
-    italic = {
-        strings = true,
-        operators = true,
-        comments = true,
-        folds = true,
-    },
-    strikethrough = true,
-    invert_selection = false,
-    invert_signs = false,
-    invert_tabline = false,
-    invert_intend_guides = false,
-    inverse = true,
-    contrast = "",
-    palette_overrides = {},
-    overrides = {},
-    dim_inactive = false,
-    transparent_mode = false,
-})
-vim.o.background = "dark"
-vim.cmd([[colorscheme gruvbox]])
-
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>f', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>s', function()
-        builtin.grep_string({ search = vim.fn.input("Grep > ")});
-end)
+lvim.builtin.terminal.open_mapping = "<c-t>"
