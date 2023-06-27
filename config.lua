ad1d93f366d93d5f2becced313e1d4f85d86bd02
Alex <shanin.x@gmail.com>
2023-06-27 23:40:08 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,3 +1,7 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
 -- vim options
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
@@ -10,6 +14,7 @@ lvim.log.level = "info"
 lvim.format_on_save = {
     enabled = true,
     pattern = "*.lua, *.go, *.c, *.cpp, *.js, *.ts",
+    "*.py",
     timeout = 1000,
 }
 -- to disable icons and use a minimalist setup, uncomment the following
@@ -23,8 +28,7 @@ lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 lvim.keys.normal_mode["<leader><tab>"] = "<cmd>DockerToolsToggle<cr>"
 
 -- Change theme settings
-lvim.colorscheme = "tokyonight-night"
-
+--
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.terminal.active = true
@@ -38,7 +42,54 @@ lvim.builtin.treesitter.auto_install = true
 -- Additional Plugins <https://www.lunarvim.org/docs/plugins#user-plugins>
 lvim.plugins = {
     { 'akinsho/toggleterm.nvim' },
-    { 'folke/tokyonight.nvim' },
-    { 'kkvh/vim-docker-tools' }
+    { 'kkvh/vim-docker-tools' },
+    { 'terrortylor/nvim-comment' },
+    { 'VonHeikemen/lsp-zero.nvim' },
+    { 'ray-x/go.nvim' },
 }
 lvim.builtin.terminal.open_mapping = "<c-t>"
+
+-- LSP
+local lsp = require('lsp-zero').preset({
+    name = 'minimal',
+    set_lsp_keymaps = true,
+    manage_nvim_cmp = true,
+    suggest_lsp_servers = false,
+})
+
+-- (Optional) Configure lua language server for neovim
+lsp.nvim_workspace()
+
+
+-- go to definition
+lsp.on_attach(function(client, bufnr)
+    local opts = { buffer = bufnr, remap = false }
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+end)
+
+lsp.setup()
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+    vim.lsp.diagnostic.on_publish_diagnostics, {
+        signs = false,
+        virtual_text = true,
+        underline = false,
+    }
+)
+
+require("nvim_comment").setup({
+    operator_mapping = "<leader>/"
+})
+
+
+-- golang
+require('go').setup()
+local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*.go",
+    callback = function()
+        require('go.format').goimport()
+    end,
+    group = format_sync_grp,
+})
+
